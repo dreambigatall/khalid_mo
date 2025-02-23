6c141e59827f5919e5a7f6e5b1e42546e246915a
dreambigatall <halidmahammed.16.hm@gmail.com>
2025-02-23 12:11:02 +0300

    latest update

--- src/projectData.js
+++ src/projectData.js
@@ -6,6 +6,46 @@ import ethioFastFood from "./assets/projectCover/ethio_fast_food.png";
 import ticTacToe from "./assets/projectCover/tictactoe.png";
 export const projects = [
   {
+    name: "Trucker",
+    image: 'https://znikiymccixmabrhrcux.supabase.co/storage/v1/object/public/storage//New_Website_Blue_Mockup_Instagram_-_Laptop__1_-removebg-preview%20(1).png',
+    projectLink: "https://dreambigatall.github.io/CapStone_project",
+    githubLink: "https://github.com/dreambigatall/CapStone_project",
+    description:
+      "Trucker is a modern truck logistics platform designed to streamline booking management. Managers gain real-time control over user bookings through a centralized dashboard. Leveraging TanStack Query for robust remote data management and React Hook Form for seamless validation, Trucker ensures a smooth and efficient user experience. The platform is powered by a scalable Supabase backend, providing reliable data storage and API capabilities",
+    skills: [
+      
+      
+      
+      "react",
+      "supabase",
+      "react hook form",
+      "tailwind css",
+      "react router",
+      "javaScript",
+      "tanstack query",
+      "react hot toast",
+  
+    ],
+  },
+  {
+    name: 'Symergygh LTD',
+    image: i4sight,
+    projectLink: 'http://symergygh.com',
+    githubLink: '',
+    description:
+      'Symergy aims to streamline and systemize the waste management and energy problems in Ghana. Recognizing the inefficiencies of current waste collection entities in accurately identifying disposal locations and the lack of standardized pricing.',
+    skills: ['Next js', 'Tailwind css', 'Typescript', 'Google Map', 'Firebase'],
+  },
+  {
+    name: "World Fusion",
+    image: worldFusion,
+    projectLink: "https://dreambigatall.github.io/World_Fusion/",
+    githubLink: "https://github.com/dreambigatall/World_Fusion",
+    description:
+      "World Fusion is a frontend application that functions essentially as a  geography quiz and map also AI integration with gemini api, allowing users to play with map and Ai to know more about the country they selected and it include geography quiz game. Redux is utilised to manage the application state.",
+    skills: ["html", "javaScript", "react", "redux", "gemini api","tailwind css", "react router", "react leaflet"],
+  },
+  {
     name: "EthioFastFood",
     image: ethioFastFood,
     projectLink: "https://dreambigatall.github.io/EthioFastFood/",
@@ -24,24 +64,8 @@ export const projects = [
   
     ],
   },
-  {
-    name: "World Fusion",
-    image: worldFusion,
-    projectLink: "https://dreambigatall.github.io/World_Fusion/",
-    githubLink: "https://github.com/dreambigatall/World_Fusion",
-    description:
-      "World Fusion is a frontend application that functions essentially as a  geography quiz and map also AI integration with gemini api, allowing users to play with map and Ai to know more about the country they selected and it include geography quiz game. Redux is utilised to manage the application state.",
-    skills: ["html", "javaScript", "react", "redux", "gemini api","tailwind css", "react router", "react leaflet"],
-  },
-  {
-    name: 'Symergygh LTD',
-    image: i4sight,
-    projectLink: 'http://symergygh.com',
-    githubLink: '',
-    description:
-      'Symergy aims to streamline and systemize the waste management and energy problems in Ghana. Recognizing the inefficiencies of current waste collection entities in accurately identifying disposal locations and the lack of standardized pricing.',
-    skills: ['Next js', 'Tailwind css', 'Typescript', 'Google Map', 'Firebase'],
-  },
+  
+  
   {
     name: 'Tic Tac Toe',
     image: ticTacToe,
